a3b4a872613150e5cbe804ba5cd070aba808c326
alexskrn <askrn123@gmail.com>
2020-08-12 18:31:39 +0300

    Changed default output to plain text and t-flag output to text with changes

--- prereform2modern/translit_from_string.py
+++ prereform2modern/translit_from_string.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# 18.01.2017 12:50:03 MSK
-
-import re
 import sys
 
 try:
@@ -11,42 +8,30 @@ try:
 except ImportError:
     from process import Processor
 
+
 def main(args=sys.argv):
-    print_log = False
-    # print(args)
     if args[1] == '-t':
         text = ' '.join(args[2:])
-        show_json = True
-    else:
-        text = ' '.join(args[1:])
-        show_json = False
-    try:
         new_txt = Processor.process_text(
             text,
-            [u'@', u'{', u'}'],
-            [u'', u'{', u'}'],
-            1,
-            print_log
+            show=True,
+            delimiters=[u'', u'{', u'}'],
+            check_brackets=False,
+            print_log=False
             )
-    except:
-        try:
-            text = text.decode('utf-8')
-            new_txt = Processor.process_text(
-                text,
-                [u'@', u'{', u'}'],
-                [u'', u'{', u'}'],
-                1,
-                print_log
-                )
-        except:
-            return 0
-    if show_json:
-        print(new_txt[-1])
     else:
-        print(new_txt[0])
+        text = ' '.join(args[1:])
+        new_txt = Processor.process_text(
+            text,
+            show=False,
+            delimiters=False,
+            check_brackets=False,
+            print_log=False
+            )
+    print(new_txt[0])
 
     return 0
 
+
 if __name__ == '__main__':
-    # main(sys.argv)
     main()
